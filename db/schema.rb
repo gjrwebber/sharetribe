a6e9d1ebf8ea38c31ac593575f043cf54ac466d5
Olli Vanhapiha <olli.vanhapiha@gmail.com>
2014-12-04 22:57:21 +0200

    TransactionService settings API

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141203095726) do
+ActiveRecord::Schema.define(:version => 20141204084648) do
 
   create_table "auth_tokens", :force => true do |t|
     t.string   "token"
